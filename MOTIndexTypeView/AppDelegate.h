f56373394cdf1f3300e2de1016d88a83ab299487
Monsterk <baoenzhao1990@163.com>
2017-07-14 10:44:05 +0800

    Initial Commit

---
+++ MOTIndexTypeView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MOTIndexTypeView
+//
+//  Created by orztech on 17/7/14.
+//  Copyright © 2017年 orztech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MOTIndexTypeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MOTIndexTypeView
+//
+//  Created by orztech on 17/7/14.
+//  Copyright © 2017年 orztech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
